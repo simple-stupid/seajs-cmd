8bf7d43d4af898e3d7e611f1d88c4545c650406e
nimojs <imhadley@163.com>
2015-04-15 21:03:14 +0800

    change dirname : spm => cmd

--- spm/src/0.0.0/index-debug.js
+++
@@ -1,6 +0,0 @@
-define("src/0.0.0/index-debug", [], function(require, exports, module){
-var src;
-
-module.exports = src;
-
-});
--- spm/src/0.0.0/index.js
+++
@@ -1,1 +0,0 @@
-define("src/0.0.0/index",[],function(e,n,i){var r;i.exports=r});
