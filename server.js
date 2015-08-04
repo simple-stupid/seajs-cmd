b92cfb0fc28155fe293e31ecba5eeb17450e1470
nimojs <imhadley@163.com>
2015-08-04 16:49:37 +0800

    change cmd server config

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var path = require('path');
 var connect = require('connect');
 
 var dir = path.join(__dirname, '/');
-
+var port = 2001
 var app = connect()
   .use(combo({
     directory: dir,
-    proxy: 'http://seajs.nimojs.com',
+    proxy: 'http://cmd.emaildm.net',
     cache: true,
     //log: true,
     static: true
   }))
-  .listen(18080);
+  .listen(2001);
+console.log('http://127.0.0.1:' + port)
